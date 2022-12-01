abd53ceb21ff746d9cd22f4b28b24c2557d72a4c
1stChaS <chananthida.so@ku.th>
2022-12-01 21:12:10 +0700

    comment 1st

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import csv
-import sys
 from turtle import Shape, Screen, Turtle
 from PIL import Image, ImageTk
-from Taonap_Bill import Customer
+from Database import Customer
+from Taonap_Bill import Bill
 from Rooms import Rooms
 from Dessert import Dessert
 from Books import Books
-sys.setrecursionlimit(1500)
 
-# better version change to def
+
 room_info = []
 choice_room = input("Do you want to book or cancel a room? (Book/Cancel): ")
 while choice_room != "Book" and choice_room != "Cancel":
+    # If customer do not input Book or Cancel.
     print("please choose again. 🥹")
     choice_room = input("Do you want to book or cancel a room? (Book/Cancel): ")
-if choice_room == "Cancel":
-    img = Image.open('facts-turtles.jpg')
-    img.show()
-    # looking in csv file
-
 
 if choice_room == "Book":
     print("")
@@ -27,74 +22,74 @@ if choice_room == "Book":
     print("")
     floor = input("What floor do you want to book? (1/2): ")
     while floor != "1" and floor != "2":
+        # If customer choose another choice which not 1 or 2.
         print("Sorry, but we have only 2 floors.")
         floor = input("What floor do you want to book? (1/2): ")
 
     if floor == "1":
-        # Screen → show a room of floor 1
-        wn = Screen()
-        wn.setup(width=500, height=650, startx=100, starty=100)
-        wn.bgcolor('white')
+        # Screen → show rooms in floor 1
+        screen = Screen()
+        screen.setup(width=500, height=650, startx=100, starty=100)
+        screen.bgcolor('white')
         img = Image.open("Floor1.png")
-        # w, h = img.size
-        w = int(img.width * 0.4)
-        h = int(img.height * 0.4)
-        img = img.resize((w, h))
+        size_w = int(img.width * 0.4)
+        size_h = int(img.height * 0.4)
+        img = img.resize((size_w, size_h))
         pic = ImageTk.PhotoImage(img)
-        wn.addshape('pic', Shape("image", pic))
+        screen.addshape('pic', Shape("image", pic))
         tr = Turtle("pic")
+
         select_room = input("What room do you want?: ")
         customer_select = Rooms(select_room, floor)
         boolean = customer_select.search()
+        # Search a room's name inside csv file.
         while not boolean:
+            # If it does not find the room's name inside csv file.
             select_room = input("What room do you want?: ")
             customer_select = Rooms(select_room, floor)
             boolean = customer_select.search()
-        wn.bye()
+        screen.bye()
 
     if floor == "2":
-        # Screen → show a room of floor 2
-        wn = Screen()
-        wn.setup(width=500, height=650, startx=100, starty=100)
-        wn.bgcolor('white')
-        wn.update()
-
+        # Screen → show rooms in floor 2
+        screen = Screen()
+        screen.setup(width=500, height=650, startx=100, starty=100)
+        screen.bgcolor('white')
+        screen.update()
         img = Image.open("Floor2.png")
         # w, h = img.size
-        w = int(img.width * 0.4)
-        h = int(img.height * 0.4)
-        img = img.resize((w, h))
+        size_w = int(img.width * 0.4)
+        size_h = int(img.height * 0.4)
+        img = img.resize((size_w, size_h))
         pic = ImageTk.PhotoImage(img)
-        wn.addshape('pic', Shape("image", pic))
+        screen.addshape('pic', Shape("image", pic))
         tr = Turtle("pic")
+
         select_room = input("What room do you want?: ")
         customer_select = Rooms(select_room, floor)
         boolean = customer_select.search()
         while not boolean:
-            # check boolean
+            # If it does not find the room's name inside csv file.
             select_room = input("What room do you want?: ")
             customer_select = Rooms(select_room, floor)
             boolean = customer_select.search()
-        wn.bye()
+        screen.bye()
 
+    # How long customer want to book a room?
     hours = int(input("How long do you want to book?(hour(s)): "))
-    if isinstance(hours, int):
-        customer_select = Rooms(select_room, floor)
-        # don't show this just check
-        price = customer_select.price() * hours
-
-    else:
-        raise ValueError("Hour(s) should be numeric.")
+    customer_select = Rooms(select_room, floor)
+    price = customer_select.price() * hours
     room_info.append([floor, select_room, hours, price])
     print("")
 
     # Dessert
-
     choice_dessert = input("Do you want any dessert?(Yes/No): ")
     dessert_info = []
     while choice_dessert != "Yes" and choice_dessert != "No":
+        # If the customer does not choose Yes or No.
         print("please choose again. 🥹")
         choice_dessert = input("Do you want any dessert?(Yes/No): ")
+
     while choice_dessert == "Yes":
         # Screen → The menu of desserts
         with open("Dessert.csv", "r") as file:
@@ -102,38 +97,46 @@ if choice_room == "Book":
             print("----------------------------------------------------------")
             print(f"{'ʕ •ᴥ•ʔゝ☆  Dessert  ʕ·ᴥ·　ʔ':^60}")
             print("----------------------------------------------------------")
+
             for i in dessert_list:
+                # Get desserts information from csv file.
                 print(f"{i['list']:^3}|  {i['menu']:<40} {i['price']:>8}.-     ")
             print("----------------------------------------------------------")
+
         select_dessert = input("Which one do you want to order?(1-10): ")
         dessert = Dessert(select_dessert)
         check = dessert.search()
-        # go to search function in dessert.py
+        # Go to search function in dessert.py, search dessert list.
+
         while not check:
+            # If customer's order is not 1 to 10.
             select_dessert = input("Which one do you want to order?(1-10): ")
             dessert = Dessert(select_dessert)
             check = dessert.search()
         order = int(input("How many orders do you want?: "))
         print(f"Great choice, You order {order} {check}.")
-        # don't show this just check
         price = dessert.price()
         dessert_info.append([check, order, price * order])
         print("")
         choice_dessert = input("Do you want more dessert(Yes/No): ")
+
         while choice_dessert != "Yes" and choice_dessert != "No":
+            # If the customer does not choose Yes or No.
             print("please choose again. 🥹")
             choice_dessert = input("Do you want any dessert?(Yes/No): ")
+
     if choice_dessert == "No":
         pass
     print("")
 
-    # # Book
-
+    # Book
     choice_book = input("Do you want any interesting books?(Yes/No): ")
     book_info = []
     while choice_book != "Yes" and choice_book != "No":
+        # If the customer does not choose Yes or No.
         print("please choose again. 🥹")
         choice_book = input("Do you want any interesting books?(Yes/No): ")
+
     while choice_book == 'Yes':
         # Screen → The List of the books
         with open("Books.csv", "r") as file:
@@ -141,7 +144,9 @@ if choice_room == "Book":
             print("----------------------------------------------------------")
             print(f"{'⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆ Books ⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆':^60}")
             print("----------------------------------------------------------")
+
             for i in data_books:
+                # Get book information from csv file.
                 print("")
                 print(f"{i['order']:^3}| {i['book_name']:<51}")
                 print(f"by {i['author']:<35}")
@@ -149,21 +154,25 @@ if choice_room == "Book":
             select_book = input("Which one do you want to read?(1-20): ")
             book = Books(select_book)
             check_book = book.search()
+
             while not check_book:
+                # If customer's order is not 1 to 20.
                 select_book = input("Which one do you want to read?(1-20): ")
                 book = Books(select_book)
                 check_book = book.search()
+
             if check_book:
                 print(f"Ok, You want to read '{check_book[0]}' by '{check_book[1]}'.")
                 book_info.append([check_book[0], check_book[1]])
-                # try to do it again every round should delete book which a customer choose from the menu.
-                # Screen → Delete number 4 book. or maybe delete in list is better!
-                # Screen → Delete number 4 book.
+
             print("")
             choice_book = input("Do you want more interesting books?(Yes/No): ")
+
             while choice_book != "Yes" and choice_book != "No":
+                # If the customer does not choose Yes or No.
                 print("please choose again. 🥹")
                 choice_book = input("Do you want any interesting books?(Yes/No): ")
+
     if choice_book == "No":
         pass
     print("")
@@ -174,7 +183,15 @@ if choice_room == "Book":
     customer_name = input("What is your name?: ")  # Chananthida Sopaphol
     customer_nickname = input("What is your nickname?: ")  # First
     customer_phone = input("What is your phone number?: ")  # 0981027726
-    customer_info = Customer(customer_name, customer_nickname, customer_phone, room_info, dessert_info, book_info)
-    bill = customer_info.print_bill()  # Bill
-    # Screen → TAO NAP CARD with name, nickname and phone number
-    # Screen → The bill of the things that you book.
+    customer_info = Bill(customer_name, customer_nickname, customer_phone, room_info, dessert_info, book_info)
+    customer_info.store()
+    # Store customer information to json file.
+    database = Customer(customer_name)
+    customer_info.print_bill()
+    # Create Bill to print to each customer.
+
+
+if choice_room == "Cancel":
+    name = input("What is your name?: ")
+    database = Customer(name)
+    find = database.cancle(name)
